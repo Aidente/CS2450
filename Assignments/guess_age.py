910776dc3c7c78800b593bc24777fbdbc2e573cf
Aidente <aiden.teduits@utahtech.edu>
2026-01-20 10:12:41 -0700

    called guess_age

--- Assignments/guess_age.py
+++ Assignments/guess_age.py
@@ -2,10 +2,11 @@ def guess_age():
     print("Hi, I'm trying to guess your age")
     name = input("Name : ")
     for i in range(15,31):
-        answer = input(f"Your age is {i}")
-        if lower(answer) == "y":
+        answer = input(f"Your age is {i}: ")
+        if answer == "y":
             print(f"{name} is {i} years old")
             break
         else:
             print("Rats")
 
+guess_age()
